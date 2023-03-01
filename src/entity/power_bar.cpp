29686481af6cf3d09ca235e3b508e867903c493d
Bondrusiek <przemekkth@outlook.com>
2023-03-01 14:44:14 +0100

    Differ colors for power_bar value.

--- src/entity/power_bar.cpp
+++ src/entity/power_bar.cpp
@@ -48,9 +48,19 @@ void PowerBar::draw(sf::RenderTarget &target, sf::RenderStates states) const
     fgSprite.setTexture(foregroundTex);
     fgSprite.setOrigin(getOrigin());
     fgSprite.setPosition(getPos().x+4, getPos().y+4+32-32*getFGScale().y);
-    //fgSprite.setTextureRect(sf::IntRect(0,0,32,))
-    //fgSprite.setTextureRect()
-    //std::cout << "y " << fgSprite.getPosition().y << std::endl;
+    if( getFGScale().y < 0.25f)
+    {
+        fgSprite.setColor(sf::Color::Green);
+    }
+    else if( getFGScale().y > 0.25 && getFGScale().y < 0.75)
+    {
+
+        fgSprite.setColor(sf::Color::Yellow);
+    }
+    else
+    {
+        fgSprite.setColor(sf::Color::Red);
+    }
 
     fgSprite.setScale(getFGScale());
     fgSprite.setRotation(getAngle());
