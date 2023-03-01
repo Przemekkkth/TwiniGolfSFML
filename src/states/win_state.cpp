cfc1e227765e4bca14b27381cc0394436da7e657
Bondrusiek <przemekkth@outlook.com>
2023-03-01 13:07:23 +0100

    Create Win state

--- src/states/win_state.cpp
+++ src/states/win_state.cpp
@@ -1,18 +1,48 @@
 #include "win_state.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "../utils/resource_holder.h"
 
 WinState::WinState(StateStack& stack, Context context)
-: State(stack, context)
+    : State(stack, context), mShowText(true), mTextEffectTime(sf::Time::Zero)
 {
+    mTitleSprite.setTexture(context.textures->get(Textures::PolyominoString));
+    mTitleSprite.setPosition(30, 30);
 
+    mInfoText.setFont(context.fonts->get(Fonts::Main));
+    mInfoText.setCharacterSize(32);
+    mInfoText.setFillColor(sf::Color::White);
+    mInfoText.setString("          Congratulation !!! \n"
+                        "You are master of Twini-Golf");
+    mInfoText.setOrigin(mInfoText.getLocalBounds().width/2,
+                        mInfoText.getLocalBounds().height/2);
+    mInfoText.setPosition(320, 240);
+
+    mPressText.setFont(context.fonts->get(Fonts::Main));
+    mPressText.setCharacterSize(24);
+    mPressText.setString("Press any key to go menu");
+    mPressText.setOrigin(mPressText.getLocalBounds().width/2.0f,
+                         mPressText.getLocalBounds().height/2.0f);
+    mPressText.setPosition(320, 400);
 }
 
 void WinState::draw()
 {
-
+    sf::RenderWindow* renderWindow = getContext().window;
+    renderWindow->draw(mTitleSprite);
+    renderWindow->draw(mInfoText);
+    if (mShowText)
+        renderWindow->draw(mPressText);
 }
 
 bool WinState::update(sf::Time dt)
 {
+    mTextEffectTime += dt;
+
+    if (mTextEffectTime >= sf::seconds(0.5f))
+    {
+        mShowText = !mShowText;
+        mTextEffectTime = sf::Time::Zero;
+    }
 
     return true;
 }
--- src/states/win_state.h
+++ src/states/win_state.h
@@ -1,6 +1,8 @@
 #ifndef WINSTATE_H
 #define WINSTATE_H
 #include "state.h"
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
 
 class WinState : public State
 {
@@ -9,6 +11,14 @@ public:
     virtual void		draw();
     virtual bool		update(sf::Time dt);
     virtual bool		handleEvent(const sf::Event& event);
+
+private:
+    sf::Sprite mTitleSprite;
+    sf::Text   mInfoText;
+    sf::Text   mPressText;
+
+    bool				mShowText;
+    sf::Time			mTextEffectTime;
 };
 
 #endif // WINSTATE_H
