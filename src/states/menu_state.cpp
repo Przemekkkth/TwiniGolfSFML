618c31bda64270fa4c0e4d67ffbd08961004895b
Bondrusiek <przemekkth@outlook.com>
2023-03-01 12:18:04 +0100

    create menu_state v1

--- src/states/menu_state.cpp
+++ src/states/menu_state.cpp
@@ -20,7 +20,7 @@ MenuState::MenuState(StateStack& stack, Context context)
 
     auto monominoButton = std::make_shared<GUI::Button>(context);
     monominoButton->setPosition(230, 200);
-    monominoButton->setText("Monomino");
+    monominoButton->setText("Play");
     monominoButton->setCallback([this] ()
     {
         GameState::choosenLevel = 1;
@@ -28,66 +28,9 @@ MenuState::MenuState(StateStack& stack, Context context)
         requestStackPush(States::Game);
     });
 
-    auto dominoButton = std::make_shared<GUI::Button>(context);
-    dominoButton->setPosition(230, 275);
-    dominoButton->setText("Domino");
-    dominoButton->setCallback([this] ()
-    {
-        GameState::choosenLevel = 2;
-        requestStackPop();
-        requestStackPush(States::Game);
-    });
-
-    auto triominoButton = std::make_shared<GUI::Button>(context);
-    triominoButton->setPosition(230, 350);
-    triominoButton->setText("Triomino");
-    triominoButton->setCallback([this] ()
-    {
-        GameState::choosenLevel = 3;
-        requestStackPop();
-        requestStackPush(States::Game);
-    });
-
-    auto tetrominoButton = std::make_shared<GUI::Button>(context);
-    tetrominoButton->setPosition(230, 425);
-    tetrominoButton->setText("Tetromino");
-    tetrominoButton->setCallback([this] ()
-    {
-        GameState::choosenLevel = 4;
-        requestStackPop();
-        requestStackPush(States::Game);
-    });
-
-    auto pentominoButton = std::make_shared<GUI::Button>(context);
-    pentominoButton->setPosition(230, 500);
-    pentominoButton->setText("Pentomino");
-    pentominoButton->setCallback([this] ()
-    {
-        GameState::choosenLevel = 5;
-        requestStackPop();
-        requestStackPush(States::Game);
-    });
-
-    auto musicButton = std::make_shared<GUI::Button>(context);
-    musicButton->setPosition(20, 575);
-    musicButton->setText("Music");
-    musicButton->setCallback([this, musicButton](){
-
-
-    });
-
-
-    auto soundButton = std::make_shared<GUI::Button>(context);
-    soundButton->setPosition(230, 575);
-    soundButton->setText("Sound");
-    soundButton->setCallback([soundButton] ()
-    {
-
-    });
-
 
     auto exitButton = std::make_shared<GUI::Button>(context);
-    exitButton->setPosition(440, 575);
+    exitButton->setPosition(230, 275);
     exitButton->setText("Exit");
     exitButton->setCallback([this] ()
     {
@@ -97,12 +40,6 @@ MenuState::MenuState(StateStack& stack, Context context)
 
 
     mGUIContainer.pack(monominoButton);
-    mGUIContainer.pack(dominoButton);
-    mGUIContainer.pack(triominoButton);
-    mGUIContainer.pack(tetrominoButton);
-    mGUIContainer.pack(pentominoButton);
-    mGUIContainer.pack(musicButton);
-    mGUIContainer.pack(soundButton);
     mGUIContainer.pack(exitButton);
 
 }
